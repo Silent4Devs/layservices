ee2310316a9234f94ce1e867aad7a90a7293c6ee
jonathan.guevara@silent4business.com <jonathan.guevara@silent4business.com>
2025-03-25 13:58:43 -0600

    Pipeline for alarm fetching

---
+++ app/pipelines/alarms_fetcher.py
@@ -0,0 +1,47 @@
+from prefect import flow, task, get_run_logger
+from app.services.apis.LogRhythmFetcher import LoghRhythmAlarmFetcher
+from app.services.apis.PRTGFetcher import PRTGAlarmFetcher
+from config import settings
+
+@task
+async def fetch_alarms_logrhythm(url: str, api_key: str):
+    
+
+    fetcher = LoghRhythmAlarmFetcher(url, api_key)
+    await fetcher.fetchAlarms()
+
+@flow
+async def alarm_monitoring_logrythm():
+    """Prefect Flow to fetch alarms in real time from Logrhythm."""
+
+    logger = get_run_logger()
+    logger.info("Escuchando alarmas de logrhythm")
+
+    url = settings.LOGRHYTHM_API_URL + "lr-alarm-api/alarms"
+    api_key = settings.LOGRHYTHM_API_TOKEN
+
+    if not api_key:
+
+        print("There is no api key in your .env")
+
+    await fetch_alarms_logrhythm(url, api_key)
+
+@task 
+async def fetch_alarms_prtg(url : str, api_key : str):
+    
+    fetcher = PRTGAlarmFetcher(url, api_key)
+    await fetcher.fetchAlarms()
+
+@flow
+async def alarm_monitoring_prtg():
+    """Prefect Flow to fetch alarms in real time from PRTG."""
+
+    logger = get_run_logger()
+    logger.info("Escuchando alarmas de prtg...")
+
+    url = settings.PRTG_API_URL
+    api_key =   settings.PRTG_API_TOKEN
+    if not api_key:
+        print("API_KEY no encontrada en .env")
+        return
+    await fetch_alarms_prtg(url, api_key)
--- app/services/apis/LogRhythmFetcher.py
+++ app/services/apis/LogRhythmFetcher.py
@@ -2,7 +2,13 @@ import aiohttp
 import asyncio
 import os
 from dotenv import load_dotenv
-from .AlarmFetcher import AlarmFetcher
+from app.services.apis.AlarmFetcher import AlarmFetcher
+from app.services.apis.LogRhythmProcessor import LogrhythmProcessor
+from config import settings
+from prefect import get_run_logger
+from app.database.Postgres import PostgreSQLManager
+from app.models.LogrhythmAlarm import LogRhythmAlarm
+from sqlalchemy.sql import exists
 
 class LoghRhythmAlarmFetcher(AlarmFetcher):
 
@@ -10,36 +16,79 @@ class LoghRhythmAlarmFetcher(AlarmFetcher):
         super().__init__(url, api_key)
         self.seen_alerts = set()
         self.headers = {"Authorization": f"Bearer {self.api_key}", "accept" : "application/json"}
+        self.processor = LogrhythmProcessor()
+        self.logger = get_run_logger()
+        self.session = PostgreSQLManager().get_client()
 
     async def fetchAlarms(self):
 
         async with aiohttp.ClientSession() as session:
             while True:
-                async with session.get(url=self.url, headers=self.headers, ssl=False) as response:
-                    print("Consultando...")
-                    if response.status == 200:                     
-                        data = await response.json()
-                    
-                        data = data.get("alarmsSearchDetails")
-                                          
-                        for alarma in data:                         
-                     
-                            alarm_id = alarma.get("alarmId") 
-                            if alarm_id not in self.seen_alerts:                             
-                                self.seen_alerts.add(alarm_id)                            
-                                print(f"Nueva alarma: {alarma}") 
-                    await asyncio.sleep(0.3)
+                try:
+                    async with session.get(url=self.url, headers=self.headers, ssl=False) as response:
+                        self.logger.info("Consultando...")
+
+                        if response.status == 200:
+                            data = await response.json()
+                            data = data.get("alarmsSearchDetails", [])
+
+                            for alarma in data:
+                                alarm_id = alarma.get("alarmId")
+                                if alarm_id not in self.seen_alerts:
+                                    self.seen_alerts.add(alarm_id)
+
+                                    try:
+                                        detail = await self.getAlarmInformationById(alarm_id)
+                                        event = await self.getAlarmEventInformationById(alarm_id)
+                                        self.processor.process({'data': data, 'detail': detail, 'event' : event})
+                                    except Exception as e:
+                                        print(f"Error procesando alarma {alarm_id}: {e}")
+
+                        else:
+                            print(f"Error HTTP: {response.status}")
+
+                except aiohttp.ClientError as e:
+                    print(f"Error de conexión: {e}")
+
+                except Exception as e:
+                    print(f"Error inesperado: {e}")
+
+                await asyncio.sleep(0.3) 
+                   
         return response.text
+    
+    async def getAlarmInformationById(self, alarm_id):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=self.url + f"/{alarm_id}", headers=self.headers, ssl=False) as response:
+                
+                if response.status == 200:
+                    
+                    data = await response.json()
+                    return data
+                
+                return
+    
+    async def getAlarmEventInformationById(self, alarm_id):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=self.url + f"/{alarm_id}/events", headers=self.headers, ssl=False) as response:
+                
+                if response.status == 200:
+                    
+                    data = await response.json()
+                    return data
+                
+                return
+            
+    def isNewAlarm(self, alarm_id):
+        """ Verifies if the alarms is already in the database"""
+        try:
+          
+            exists_query = self.session.query(
+                exists().where(LogRhythmAlarm.alarm_id == alarm_id)
+            ).scalar()
+            
+            return not exists_query
 
-async def main():
-    load_dotenv()
-    url = os.getenv("LOGRHYTHM_API_TOKEN") + "lr-alarm-api/alarms"
-    api_key = os.getenv("LOGRHYTHM_API_TOKEN")
-    if not api_key:
-        print("API_KEY no encontrada en .env")
-        return
-    fetcher = LoghRhythmAlarmFetcher(url, api_key)
-    await fetcher.fetchAlarms()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        except Exception as e:
+            self.logger.error(f"Error verificando alarma: {str(e)}")
+            raise
