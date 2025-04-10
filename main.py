c9235d2efce0feda73a063c51a738240182e2ad4
jonathan.guevara@silent4business.com <jonathan.guevara@silent4business.com>
2025-04-09 20:14:22 -0600

    Add /test route for fastapi application

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ from transformers import pipeline, GPT2LMHeadModel, GPT2Tokenizer
 from app.routes import health_databases
 from app.routes import documents
 import torch
+from contextlib import asynccontextmanager
+from app.services.StartUpProcess import starting
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    starting()  
+    yield  
+    print("Cerrando aplicación...")  
 
 app = FastAPI(
     title="SOCfusion",
     description="AI Agent for Automating Tasks and Decision-Making in SOC",
-    version="0.1.0"
+    version="0.1.0", 
+    lifespan=lifespan
 )
 
 app.include_router(health_databases.router, prefix="/health")
 app.include_router(documents.router, prefix="/documents")
+
+# Use this route to test code and functions
+@app.get("/test")
+def test_route():
+    print("Hola mundo")
+    return {"message": "Test endpoint activo"}
+
 
 if __name__ == "__main__":
     import uvicorn
