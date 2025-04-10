4b6a0f1ca04296476fa564baf415ad54602ffe8c
jonathan.guevara@silent4business.com <jonathan.guevara@silent4business.com>
2025-04-09 18:39:23 -0600

    Optimize MCP server

--- app/services/Docling.py
+++ app/services/Docling.py
@@ -10,10 +10,13 @@ from docling.document_converter import (
 )
 from docling.pipeline.simple_pipeline import SimplePipeline
 from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
-
+from io import BytesIO
+import tempfile
+from pathlib import Path
+from typing import List
 
 class DocumentProcessor:
-    def __init__(self, input_paths, output_dir="scratch"):
+    def __init__(self, input_paths : str = "", output_dir="scratch"):
         self.input_paths = input_paths
         self.output_dir = Path(output_dir)
         self.doc_converter = self.configureDocumentConverter()
@@ -47,7 +50,7 @@ class DocumentProcessor:
         for res in conv_results:
             self.saveJson(res)
             self.saveYaml(res)
-
+            self.saveMarkdown(res)
 
     def saveMarkdown(self, res):
         md_output_path = self.output_dir / f"{res.input.file.stem}.md"
@@ -63,13 +66,43 @@ class DocumentProcessor:
         yaml_output_path = self.output_dir / f"{res.input.file.stem}.yaml"
         with yaml_output_path.open("w", encoding="utf-8") as fp:
             yaml.safe_dump(res.document.export_to_dict(), fp)
+        
+    def getMarkdown(self, file: BytesIO | bytes) -> str:
+        """
+        Converts an file (PDF, DOCX, etc.) to Markdown format and returns it as a string.
+        
+        Args:
+            file (BytesIO or bytes): The file to convert.
+        
+        Returns:
+            str: The content converted to Markdown format.
+        """
+        with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+        
+            if isinstance(file, BytesIO):
+                content = file.getvalue()
+            else:
+                content = file
+            
+            temp_file.write(content)
+            temp_file.flush()
+            
+            conv_results = self.doc_converter.convert(temp_file.name)
+            
+            if not conv_results:
+                raise ValueError("Error en la conversión")
+            
+            return conv_results.document.export_to_markdown()
+        
+    def getParagraphsFromMarkdown(self, markdown_text: str) -> List[str]:
+        """
+        Splits a Markdown string into paragraphs.
 
-if __name__ == "__main__":
+        Args:
+            markdown_text (str): The input Markdown-formatted text.
 
-
-    input_paths = [
-        Path("/home/jonathan/Downloads/herramientas.docx"),
-    ]
-  
-    processor = DocumentProcessor(input_paths)
-    processor.processDocuments()
+        Returns:
+            List[str]: A list of paragraphs.
+        """
+        paragraphs = [p.strip() for p in markdown_text.split("\n\n") if p.strip()]
+        return paragraphs
