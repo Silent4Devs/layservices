592a6f14155d199a33662f2cb79c0db23890bfe7
jonathan.guevara@silent4business.com <jonathan.guevara@silent4business.com>
2025-04-17 15:24:04 -0600

    Add alarmas endpoints

---
+++ app/routes/alarms.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException, Header, Depends
+from app.database.FileServer import SMBClient
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from typing import List
+from app.database.Postgres import PostgreSQLManager
+from app.models.LogrhythmAlarm import LogRhythmAlarm
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+def get_db():
+    db = PostgreSQLManager().get_client()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/logrhythm/")
+def get_all_alarms(db: Session = Depends(get_db)):
+    alarms = db.query(LogRhythmAlarm).order_by(LogRhythmAlarm.date_inserted).all()
+    return jsonable_encoder(alarms)
+
+@router.post("/classify")
+def classifyAlarm():
+    pass
